c6a3ab1b3c2fb2bb06259b4fee0d88e312c0b246
HarielDev <hariol.santana138@gmail.com>
2025-01-05 18:25:44 -0300

    feat: gravando no ArtigoServiceImpl a vinculacao do autor

--- src/main/java/com/fiap/springblog/service/impl/ArtigoServiceImpl.java
+++ src/main/java/com/fiap/springblog/service/impl/ArtigoServiceImpl.java
@@ -1,7 +1,9 @@
 package com.fiap.springblog.service.impl;
 
 import com.fiap.springblog.model.Artigo;
+import com.fiap.springblog.model.Autor;
 import com.fiap.springblog.repository.ArtigoRepository;
+import com.fiap.springblog.repository.AutorRepository;
 import com.fiap.springblog.service.ArtigoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,7 @@ public class ArtigoServiceImpl implements ArtigoService {
 
     @Autowired
     private ArtigoRepository artigoRepository;
+    private AutorRepository autorRepository;
 
     @Override
     public List<Artigo> obterTodos() {
@@ -28,6 +31,20 @@ public class ArtigoServiceImpl implements ArtigoService {
 
     @Override
     public Artigo criar(Artigo artigo) {
+
+        //se o autor existe
+        if (artigo.getAutor().getCodigo() != null) {
+            //recuperar o autor
+            Autor autor = this.autorRepository.findById(artigo.getAutor().getCodigo())
+                    .orElseThrow(()-> new IllegalArgumentException("Autor inexistente!!"));
+
+            artigo.setAutor(autor);
+        } else {
+            // Se nao, nao atribuir o autor ao artigo
+            artigo.setAutor(null);
+        }
+
+        //Salvo o artigo com o autor ja cadastrado
         return this.artigoRepository.save(artigo);
     }
 }
