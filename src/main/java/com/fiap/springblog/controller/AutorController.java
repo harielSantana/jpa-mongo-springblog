cff9f3cb506eba8612857a727a66abadff8b2b94
HarielDev <hariol.santana138@gmail.com>
2025-01-05 20:58:22 -0300

    fix: atualizando autor

--- src/main/java/com/fiap/springblog/controller/AutorController.java
+++ src/main/java/com/fiap/springblog/controller/AutorController.java
@@ -3,11 +3,10 @@ package com.fiap.springblog.controller;
 import com.fiap.springblog.model.Autor;
 import com.fiap.springblog.service.AutorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping(value = "/autores")
 public class AutorController {
 
     @Autowired
--- src/main/java/com/fiap/springblog/model/Autor.java
+++ src/main/java/com/fiap/springblog/model/Autor.java
@@ -5,11 +5,14 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
-@Data
 public class Autor {
     @Id
     public String codigo;
     public String nome;
     public String biografia;
     public String imagem;
+
+    public String getCodigo() {
+        return codigo;
+    }
 }
